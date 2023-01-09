fcbc28cf4a34eb11dc56387b3dc1d4ee679ad7d0
Spong3ibob <leonardomaran17@gmail.com>
2023-01-09 16:47:44 +0100

    aggiunto film

--- index.php
+++ index.php
@@ -6,31 +6,30 @@ class Movies {
     public $year;
     private $age;
 
-    public function __construct($_name, $_lenght, $_type, $_year)
+    public function __construct($_name, $_lenght, $_type, $_year, $_age)
     {
         $this -> name = $_name;
         $this -> lenght = $_lenght;
         $this -> type = $_type;
         $this -> year = $_year;
+        $this -> age = $_age;
 
     }
-
-    public function setAge($age) {
-        if($age > 18 != "vietato ai minori");
-        $this->age = $age;
+    public function setAge($age){
+        if($age < 18);
+        $this -> age = $age;
     }
+    
     public function getAge() {
-       return $this->age;
+       return $this -> age;
     }
+    
 }
 
-$avatar = new Movies("AVATAR LA VITA DELL' ACQUA","3h 12m","Sci-fi/Azione","2022" );
-// $avatar -> name = ;
-// $avatar -> lenght = ;
-// $avatar -> type = ;
-// $avatar -> year = ;
-$avatar -> setAge("27");
-var_dump($avatar)
+$avatar = new Movies("AVATAR LA VITA DELL' ACQUA","3h 12m","Sci-fi/Azione","2022",13 );
+$topGun = new Movies("TOP GUN MAVERICK", "2h 11m","Azione/Avventura", "2022",18);
+var_dump($avatar);
+var_dump($topGun);
 ?>
 
 <!DOCTYPE html>
@@ -46,7 +45,12 @@ var_dump($avatar)
     <h3>Anno di uscita: <?php echo $avatar -> year; ?></h3>
     <h3>Dutata: <?php echo $avatar -> lenght; ?></h3>
     <h2>Tipologia: <?php echo $avatar -> type; ?></h2>
-    <h2>Età: <?php echo $avatar ->getAge(); ?></h2>
+    <h2>Età: <?php echo $avatar ->getAge(); ?>+</h2>
 
+    <h1><?php echo $topGun -> name; ?></h1>
+    <h3>Anno di uscita: <?php echo $topGun -> year; ?></h3>
+    <h3>Dutata: <?php echo $topGun -> lenght; ?></h3>
+    <h2>Tipologia: <?php echo $topGun -> type; ?></h2>
+    <h2>Età: <?php echo $topGun ->getAge(); ?>+</h2>
 </body>
 </html>
